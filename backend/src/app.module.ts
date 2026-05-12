79eb84ad66291d387ab4aec660ab8a2bceb96a96
kennyasimi <kennyasimi23@gmail.com>
2026-05-13 00:23:23 +0300

    Minor fix

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,7 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
-import { PrismaModule } from './prisma/prisma.module';
 import { PrismaService } from './prisma/prisma.service';
-import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [UsersModule, AuthModule, PrismaModule],
