16d338a212d3e6cb0b3b9f7b6c6e68b26b28a2fc
kennyasimi <kennyasimi23@gmail.com>
2026-05-14 09:25:57 +0300

    Imported boat module to app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,9 +5,10 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { PrismaService } from './prisma/prisma.service';
+import { BoatsModule } from './boats/boats.module';
 
 @Module({
-  imports: [UsersModule, AuthModule, PrismaModule],
+  imports: [UsersModule, AuthModule, PrismaModule, BoatsModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
