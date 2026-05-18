20e37722160b5e2fa20e775d5632b1c31242062a
kennyasimi <kennyasimi23@gmail.com>
2026-05-19 00:40:48 +0300

    Proper importation of modules and required services

--- backend/src/payments/payments.module.ts
+++ backend/src/payments/payments.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
-import { PaymentsService } from './payments.service';
+import { paymentsService } from './payments.service';
 import { PaymentsController } from './payments.controller';
+import { BookingsService } from 'src/bookings/bookings.service';
+import { BookingsModule } from 'src/bookings/bookings.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  providers: [PaymentsService],
+  imports: [BookingsModule, ConfigModule],
+  providers: [paymentsService],
   controllers: [PaymentsController]
 })
 export class PaymentsModule {}
