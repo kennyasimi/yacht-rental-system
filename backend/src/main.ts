cf0c53a0104236a60d105f7653a55445ceb5e78a
kennyasimi <kennyasimi23@gmail.com>
2026-05-27 20:11:17 +0300

    Fix CORS issue

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,13 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-  app.enableCors({
-    origin: 'http://localhost:5173',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+    prefix: '/uploads/',
   });
+  app.enableCors();
+  await app.listen(3000);
+  
+
+
 }
 bootstrap();
