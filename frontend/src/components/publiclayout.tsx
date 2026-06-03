05ac375fd7183635ded26c89cdeafb7705f3090f
kennyasimi <kennyasimi23@gmail.com>
2026-06-03 06:46:59 +0300

    Conditional rendering between guest, logged in and admin users

--- frontend/src/components/publiclayout.tsx
+++ frontend/src/components/publiclayout.tsx
@@ -1,5 +1,6 @@
 import PublicNavbar from './publicnavbar';
 import AppNavbar from './appnavbar';
+import AdminNavbar from './adminnavbar';
 import { Outlet } from 'react-router-dom';
 type Props = {
   children: React.ReactNode;
@@ -7,15 +8,34 @@ type Props = {
 
 function MainLayout({ children }: Props) {
   const token = localStorage.getItem('token');
+
+  const getUserRole = () => {
+    if (!token) return null;
+    
+    try {
+      
+      const payload = JSON.parse(atob(token.split('.')[1]));
+      return payload.role; 
+    } catch (error) {
+      console.error('Invalid token', error);
+      return null;
+    }
+  };
+  
+  const userRole = getUserRole();
+  
+  const renderNavbar = () => {
+    if (!token) return <PublicNavbar />;
+    if (userRole === 'ADMIN') return <AdminNavbar />;
+    return <AppNavbar />;
+  };
+
+
   return (
 
     <div>
 
-      {
-        token
-          ? <AppNavbar />
-          : <PublicNavbar />
-      }
+      {renderNavbar()}
 
       <main>
 
